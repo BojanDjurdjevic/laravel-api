de71d8781e465902acd0783afe4f1bbc9df39bf2
BojanDjurdjevic <pininfarina164@gmail.com>
2026-04-26 07:25:55 +0200

    Breeze's login/register modified

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,22 +4,30 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
-class AuthenticatedSessionController extends Controller
+class LoginController extends Controller
 {
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): array
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
+        //$request->session()->regenerate(); // Ne koristimo ovo jer koristimo tokene
 
-        return response()->noContent();
+        $user = $request->user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return [
+            'user' => new UserResource($user),
+            'token' => $token
+        ];
     }
 
     /**
